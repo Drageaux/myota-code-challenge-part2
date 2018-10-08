8fdbbe7014b34bbfe7e4eec886bed097389dbd70
David Thong Nguyen <thnguyen2808@gmail.com>
2018-10-07 21:40:52 -0400

    Clean up the rest after loop ends

--- src/TimeMachineSystem.java
+++ src/TimeMachineSystem.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class TimeMachineSystem {
 
     private final static Byte BOUNDARY_BYTE = new Byte("48");
-    private final static Byte EOF_BYTE = new Byte("0");
+    private final static int MAX_FILE_SIZE = 32;
 
     private TimeMachineFile exampleFile;
 
@@ -53,30 +53,25 @@ public class TimeMachineSystem {
 
         ArrayList<Byte> currBytes = new ArrayList<>();
         // read up until boundary 0x30, or 48 decimal, or '0' ASCII
-        // or EOF, or 0 decimal, or 'EOF' ASCII
         for (byte b : data) {
-            if (b == BOUNDARY_BYTE.byteValue()) { // found border
-                if (currBytes.size() == 0) { // if empty, add to currBytes
-                    System.out.println("First chunk");
-                } else { // else, finish chunk and add to currBytes
+            if (b == this.BOUNDARY_BYTE.byteValue()) { // found border
+                if (currBytes.size() != 0) { // finish chunk and add to currBytes
                     Chunk newChunk = new Chunk(currBytes);
                     map.put(new String(String.valueOf(currBytes)), newChunk);
-
                     currBytes.clear();
                 }
-            } else if (b == EOF_BYTE.byteValue()) { // found end of file
-                System.out.println("End of file");
-                Chunk newChunk = new Chunk(currBytes);
-                map.put(new String(String.valueOf(currBytes)), newChunk);
-
-                currBytes.clear();
-                break;
             }
-
-            if (currBytes.size() <= 32) {
+            if (currBytes.size() < this.MAX_FILE_SIZE) {
                 currBytes.add(b);
             }
         }
+        if (currBytes.size() != 0) { // read and store the rest until end of array
+            System.out.println("End of file");
+            Chunk newChunk = new Chunk(currBytes);
+            map.put(new String(String.valueOf(currBytes)), newChunk);
+            currBytes.clear();
+        }
+
         System.out.println("Current bytes: " + currBytes);
 
         return false;
